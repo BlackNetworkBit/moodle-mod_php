7905f5257983cd8587e716623332de487582d234
BlackNetworkBit <vincent.schneider@gmx.net>
2016-11-03 13:04:02 +0100

    Book Chapter 12

---
+++ lang/de/php.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for mod_php.
+ *
+ * @package   mod_php
+ * @copyright 2016 Tomasz Muras  {@link https://leanpub.com/moodle}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['gradedon'] = 'Graded {$a->grade} on {$a->timegraded}';
+$string['modulename'] = 'PHP Aufgabe';
+$string['modulename_help'] = 'PHP Aufgabe ist eine Activität die Schüler nach ihren coding skills tested';
+$string['modulenameplural'] = 'PHP Aufgaben';
+$string['pluginname'] = 'PHP Aufgaben';
+$string['pluginadministration'] = 'PHP Aufgaben admin';
+$string['php:grade'] = 'Note.';
+$string['phpsubmission'] = 'PHP submission {$a}';
+$string['submittedon'] = 'Submitted on {$a}';
